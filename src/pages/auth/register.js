b4a6f55de1020b1248a294a73ef5dad546e3e868
kafeelkazeem <kafeelkazeem@gmail.com>
2024-09-16 14:22:04 +0100

    added uiverse

--- src/pages/auth/register.js
+++ src/pages/auth/register.js
@@ -1,173 +1,181 @@
-import React, { useState } from 'react';
-import { TextField, IconButton, InputAdornment, MenuItem, Select, FormControl, InputLabel } from '@mui/material';
-import { Business, Email, Phone, Home, Visibility, VisibilityOff, Lock } from '@mui/icons-material';
+import React from "react";
+import styled from "styled-components";
 
 const Register = () => {
-  const [showPassword, setShowPassword] = useState(false);
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const [serviceCategory, setServiceCategory] = useState('');
-
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-
-  const toggleConfirmPasswordVisibility = () => {
-    setShowConfirmPassword(!showConfirmPassword);
-  };
-
-  const handleServiceCategoryChange = (event) => {
-    setServiceCategory(event.target.value);
-  };
-
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <div className="h-fit p-5 flex flex-col overflow-hidden bg-white rounded-md shadow-md gap-4">
-        <h1 className="text-2xl lg:text-4xl tracking-wide text-left font-bold">Register</h1>
-        <div className="w-full flex flex-row gap-5">
-          <TextField
-            fullWidth
-            label="Business name"
-            placeholder="Business name"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Business />
-                </InputAdornment>
-              ),
-            }}
-            variant="outlined"
-          />
-          <TextField
-            fullWidth
-            label="Email"
-            placeholder="Enter your email"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Email />
-                </InputAdornment>
-              ),
-            }}
-            variant="outlined"
-          />
+    <StyledWrapper>
+      <div className="w-full h-screen flex justify-center items-center">
+      <div className="p-5 shadow-lg rounded-sm">
+      <form className="form">
+        <p className="title">Register </p>
+        <p className="message">Signup now and get full access to our app. </p>
+        <div className="flex">
+          <label>
+            <input required placeholder="" type="text" className="input" />
+            <span>Business Name</span>
+          </label>
+
+          <label>
+            <input required placeholder="" type="email" className="input" />
+            <span>Email</span>
+          </label>
         </div>
-        <FormControl fullWidth>
-          <InputLabel id="service-category-label">Select a service category</InputLabel>
-          <Select
-            labelId="service-category-label"
-            value={serviceCategory}
-            onChange={handleServiceCategoryChange}
-            label="Select a service category"
-            variant="outlined"
-          >
-            <MenuItem value="carpenter">Carpenter</MenuItem>
-            <MenuItem value="painter">Painter</MenuItem>
-            <MenuItem value="electrician">Electrician</MenuItem>
-            <MenuItem value="tailor">Tailor</MenuItem>
-            <MenuItem value="plumber">Plumber</MenuItem>
-          </Select>
-        </FormControl>
-        <div className="flex flex-row w-full gap-5">
-          <TextField
-            fullWidth
-            label="Phone Number"
-            placeholder="Phone Number"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Phone />
-                </InputAdornment>
-              ),
-            }}
-            variant="outlined"
-          />
-          <TextField
-            fullWidth
-            label="State"
-            placeholder="State"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Home />
-                </InputAdornment>
-              ),
-            }}
-            variant="outlined"
-          />
-        </div>
-        <TextField
-          fullWidth
-          label="Address"
-          placeholder="Enter your address"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <Home />
-              </InputAdornment>
-            ),
-          }}
-          variant="outlined"
-        />
-        <div className="w-full flex flex-row gap-5">
-          <TextField
-            fullWidth
-            label="Password"
-            placeholder="Password"
-            type={showPassword ? 'text' : 'password'}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Lock />
-                </InputAdornment>
-              ),
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={togglePasswordVisibility}>
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-            variant="outlined"
-          />
-          <TextField
-            fullWidth
-            label="Confirm Password"
-            placeholder="Confirm your password"
-            type={showConfirmPassword ? 'text' : 'password'}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Lock />
-                </InputAdornment>
-              ),
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={toggleConfirmPasswordVisibility}>
-                    {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-            variant="outlined"
-          />
-        </div>
-        <div>
-          <button
-            type="submit"
-            className="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md focus:outline-none hover:bg-blue-700"
-          >
-            Create account
-          </button>
-        </div>
-        <div className="text-center">
-          <p className="text-base text-gray-600">
-            Already have an account? <a href="#" className="font-medium text-orange-500 transition-all duration-200 hover:text-orange-600 hover:underline">Login here</a>
-          </p>
-        </div>
+
+        <label>
+          <input required placeholder="" type="email" className="input" />
+          <span>Email</span>
+        </label>
+
+        <label>
+          <input required placeholder="" type="password" className="input" />
+          <span>Password</span>
+        </label>
+        <label>
+          <input required placeholder="" type="password" className="input" />
+          <span>Confirm password</span>
+        </label>
+        <button className="submit">Submit</button>
+        <p className="signin">
+          Already have an acount ? <a href="#">Signin</a>{" "}
+        </p>
+      </form>
       </div>
-    </div>
+      </div>
+    </StyledWrapper>
   );
 };
+
+const StyledWrapper = styled.div`
+  .form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  max-width: 350px;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 20px;
+  position: relative;
+}
+
+.title {
+  font-size: 28px;
+  color: royalblue;
+  font-weight: 600;
+  letter-spacing: -1px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding-left: 30px;
+}
+
+.title::before,.title::after {
+  position: absolute;
+  content: "";
+  height: 16px;
+  width: 16px;
+  border-radius: 50%;
+  left: 0px;
+  background-color: royalblue;
+}
+
+.title::before {
+  width: 18px;
+  height: 18px;
+  background-color: royalblue;
+}
+
+.title::after {
+  width: 18px;
+  height: 18px;
+  animation: pulse 1s linear infinite;
+}
+
+.message, .signin {
+  color: rgba(88, 87, 87, 0.822);
+  font-size: 14px;
+}
+
+.signin {
+  text-align: center;
+}
+
+.signin a {
+  color: royalblue;
+}
+
+.signin a:hover {
+  text-decoration: underline royalblue;
+}
+
+.flex {
+  display: flex;
+  width: 100%;
+  gap: 6px;
+}
+
+.form label {
+  position: relative;
+}
+
+.form label .input {
+  width: 100%;
+  padding: 10px 10px 20px 10px;
+  outline: 0;
+  border: 1px solid rgba(105, 105, 105, 0.397);
+  border-radius: 10px;
+}
+
+.form label .input + span {
+  position: absolute;
+  left: 10px;
+  top: 15px;
+  color: grey;
+  font-size: 0.9em;
+  cursor: text;
+  transition: 0.3s ease;
+}
+
+.form label .input:placeholder-shown + span {
+  top: 15px;
+  font-size: 0.9em;
+}
+
+.form label .input:focus + span,.form label .input:valid + span {
+  top: 30px;
+  font-size: 0.7em;
+  font-weight: 600;
+}
+
+.form label .input:valid + span {
+  color: green;
+}
+
+.submit {
+  border: none;
+  outline: none;
+  background-color: royalblue;
+  padding: 10px;
+  border-radius: 10px;
+  color: #fff;
+  font-size: 16px;
+  transform: .3s ease;
+}
+
+.submit:hover {
+  background-color: rgb(56, 90, 194);
+}
+
+@keyframes pulse {
+  from {
+    transform: scale(0.9);
+    opacity: 1;
+  }
+
+  to {
+    transform: scale(1.8);
+    opacity: 0;
+  }
+}
+`;
 
 export default Register;
